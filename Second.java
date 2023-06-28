cd0561c24ccdc625797d803fba0ce8ec23be0593
AmruthaKotapuri <amruthakotpuri104@gmail.com>
2023-06-28 11:03:57 +0530

    This is my day 2 commit

---
+++ Second.java
@@ -0,0 +1,9 @@
+class Second 
+{
+    public static void main(String args[])
+    {
+        System.out.println("this is a color");
+    }
+}
+
+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
---
+++ Sum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Sum
+{
+    public static void main( String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter two numbers");
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        System.out.println("the sum is "+(a+b));
+    }
+}
